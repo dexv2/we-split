58e80011ace7d5f3283e2905833c2f8d8413e611
phil002109 <vermontphil.paguiligan@gmail.com>
2024-10-10 11:04:54 +0800

    Add dynamic total amount color

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -69,6 +69,7 @@ struct ContentView: View {
                 
                 Section("Total amount") {
                     Text(totalAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .foregroundStyle(tipPercentage == 0 ? .red : .black)
                 }
             }
             .navigationBarTitleDisplayMode(.inline)
